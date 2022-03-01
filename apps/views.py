0ce6733dcfc03a078d27277c4a4f84e2ccd98b46
Orlando N Callupe <orlando.ncallupe@gmail.com>
2022-03-01 08:46:16 -0500

    Spaces STORAGE 3.1

--- apps/views.py
+++ apps/views.py
@@ -192,7 +192,6 @@ def updatedate_normativa(request,codigo):
 
         if file_pdf != None:
             fs = FileSystemStorage()
-            # file_pdf.name = str(uuid.uuid4())
             Register_Normativa.objects.get(id=codigo).document.delete()
             filename = fs.save('Document_normativa/' + file_pdf.name, file_pdf)
             file_pdf = filename
