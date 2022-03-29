87404d28d2e3aec45adf01fb31ad436dc880ae28
suyon.jesus@gmail.com <jesus>
2022-03-29 00:09:25 -0500

    user plan profesional inactive when not have membership or not pay for that

--- apps/middleware.py
+++ apps/middleware.py
@@ -3,7 +3,8 @@ from datetime import date
 from apps.models import Order_payment
 from apps.models import Membership, Member
 from django.contrib.auth.models import User
-
+from django.contrib.auth import logout
+from django.shortcuts import redirect
 class CheckMembership:
     def __init__(self, get_response):
         self.get_response = get_response
@@ -20,6 +21,7 @@ class CheckMembership:
             identity = user.user_membership.identity
             member = Member.objects.get(identity=identity)
             orders_exists = Order_payment.objects.filter(identity=identity).exists()
+
             if type_membership=='PLPA' or type_membership=='PLPPA' :
                 if orders_exists:
                     last_order = Order_payment.objects.filter(identity=identity).latest('created')
@@ -40,6 +42,33 @@ class CheckMembership:
                     pl_agremiado=Membership.objects.get(membership_type='PLPA')
                     member.membership = pl_agremiado
                     member.save()
+
+            elif type_membership=='PLPP' or type_membership=='PPPP' :
+                #profesional plan
+                print('usuario:', user.is_active)
+
+                if orders_exists:
+                    last_order = Order_payment.objects.filter(identity=identity).latest('created')
+                    day_finish_plan = last_order.validity_date_finish
+                    actual_day = timezone.now()
+                    if actual_day > day_finish_plan:
+                        # pierde acceso
+                        user.is_active = False
+                        user.save()
+                        logout(request)
+                        return redirect('login')
+                    else:
+                        #mantener membresia profesional
+                        pl_profesional=Membership.objects.get(membership_type='PLPP')
+                        member.membership = pl_profesional
+                        member.save()
+                       
+                else:
+                    # no debe tener acceso
+                    user.is_active = False
+                    user.save()
+                    logout(request)
+                    return redirect('login')
                         
                             
         except:
