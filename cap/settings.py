8b85d914343d4cfcaf97da3a1a896c411c5c6868
Orlando N <orlando.ncallupe@gmail.com>
2022-04-11 17:58:34 -0500

    middleware membership

--- cap/settings.py
+++ cap/settings.py
@@ -62,6 +62,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'apps.middleware.CheckMembership',
 ]
 
 ROOT_URLCONF = 'cap.urls'
