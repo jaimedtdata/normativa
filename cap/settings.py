b13ef5ead4df9661b55a62f769ceaf921fd520fc
Orlando N <orlando.ncallupe@gmail.com>
2022-04-12 12:12:35 -0500

    deploy

--- cap/settings.py
+++ cap/settings.py
@@ -154,7 +154,6 @@ MEDIA_ROOT=os.path.join(BASE_DIR,'media')
 # EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 # EMAIL_SUBJECT_PREFIX='Colegio de Arquitectos del Perú'
 
-
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST=os.getenv('EMAIL_HOST')
 EMAIL_HOST_USER=os.getenv('EMAIL_HOST_USER')
@@ -167,6 +166,7 @@ EMAIL_SUBJECT_PREFIX='Colegio de Arquitectos del Perú'
 USERNAME_NIUBIZ = os.getenv('USERNAME_NIUBIZ')
 PASSWORD_NIUBIZ = os.getenv('PASSWORD_NIUBIZ')
 COMERCIAL_ID = os.getenv('COMERCIAL_ID')
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 # # Space AWS
 AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
