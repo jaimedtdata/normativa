cf4242a42105f7d71a9b07352ddaf248a728476d
jesus <suyon.jesus@gmail.com>
2022-04-28 22:23:49 -0500

    javascript button refresh dni fixed

--- static/register/js/results.js
+++ static/register/js/results.js
@@ -56,7 +56,10 @@ var base_url = window.location.origin;
                         test.innerHTML = userExists
                         document.getElementById('dni-input').disabled=true
                         document.getElementById('input-value').disabled=true
-                        n_reload.style.display='block' //button reload
+                        if(document.getElementById('input-value').disabled=true){
+
+                            document.getElementById('n_reload').style.display="block" //button reload
+                        }
                         spinnerDni.innerHTML = `<div style="display: none;" class="spinner-border text-muted"></div>`
                     }
 
@@ -109,7 +112,10 @@ var base_url = window.location.origin;
                         document.getElementById('input-value').disabled=true
                         blockExternal=document.getElementById('block-external')
                         blockExternal.classList.remove('form-hide')
-                        n_reload.style.display='block' //button reload
+                        if(document.getElementById('input-value').disabled=true){
+
+                            document.getElementById('n_reload').style.display="block" //button reload
+                        }
                         spinnerDni.innerHTML = `<div style="display: none;" class="spinner-border text-muted"></div>`
 
                         
